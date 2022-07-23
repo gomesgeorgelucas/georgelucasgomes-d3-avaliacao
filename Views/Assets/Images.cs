694374efe2a78b638f5809a9694695b3c91c712d
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-07-23 00:32:41 -0400

    feature/d3-1: main menu loop creation and styling

---
+++ Views/Assets/Images.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace georgelucasgomes_d3_avaliacao.Views.Assets
+{
+    internal static class Images
+    {
+        private static readonly string _LOGO = "                   ,----,                                                                                       \n                 ,/   .`|                                                                         .--,-``-.     \n  ,----..      ,`   .'  :   ,---,.    ,---,      .--.--.                                 ,---,   /   /     '.   \n /   /   \\   ;    ;     / ,'  .' |  .'  .' `\\   /  /    '.                             .'  .' `\\/ ../        ;  \n|   :     :.'___,/    ,',---.'   |,---.'     \\ |  :  /`. /             ,---,.        ,---.'     \\ ``\\  .`-    ' \n.   |  ;. /|    :     | |   |   .'|   |  .`\\  |;  |  |--`            ,'  .' |        |   |  .`\\  \\___\\/   \\   : \n.   ; /--` ;    |.';  ; :   :  |-,:   : |  '  ||  :  ;_            ,---.'   ,        :   : |  '  |    \\   :   | \n;   | ;    `----'  |  | :   |  ;/||   ' '  ;  : \\  \\    `.         |   |    |        |   ' '  ;  :    /  /   /  \n|   : |        '   :  ; |   :   .''   | ;  .  |  `----.   \\        :   :  .'         '   | ;  .  |    \\  \\   \\  \n.   | '___     |   |  ' |   |  |-,|   | :  |  '  __ \\  \\  |        :   |.'           |   | :  |  '___ /   :   | \n'   ; : .'|    '   :  | '   :  ;/|'   : | /  ;  /  /`--'  /        `---'             '   : | /  ;/   /\\   /   : \n'   | '/  :    ;   |.'  |   |    \\|   | '` ,/  '--'.     /                           |   | '` ,// ,,/  ',-    . \n|   :    /     '---'    |   :   .';   :  .'      `--'---'                            ;   :  .'  \\ ''\\        ;  \n \\   \\ .'               |   | ,'  |   ,.'                                            |   ,.'     \\   \\     .'   \n  `---`                 `----'    '---'                                              '---'        `--`-,,-'     \n                                                                                                                ";
+        private static readonly string _DIVIDER = new String('-', 112);
+        private static readonly string _PROMPT = "Option: ";
+
+        public static string LOGO => _LOGO;
+        public static string DIVIDER => _DIVIDER;
+        public static string PROMPT => _PROMPT;
+    }
+
+}
---
+++ Views/Assets/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace georgelucasgomes_d3_avaliacao.Views.Assets
+{
+    internal static class Messages
+    {
+        private static readonly string _MAIN_MENU_LOGIN_TITLE = "CTEDS BBS LOGIN SERVICE";
+        private static readonly string _MAIN_MENU_OPTION_LOGIN = "Login(L)";
+        private static readonly string _MAIN_MENU_OPTION_EXIT = "Exit(X)";
+
+        public static string MAIN_MENU_LOGIN_TITLE => _MAIN_MENU_LOGIN_TITLE;
+
+        public static string MAIN_MENU_OPTION_LOGIN => _MAIN_MENU_OPTION_LOGIN;
+
+        public static string MAIN_MENU_OPTION_EXIT => _MAIN_MENU_OPTION_EXIT;
+    }
+}
