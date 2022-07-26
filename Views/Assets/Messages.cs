8ab75281acb8f9d718630352c25e28421af3d2f7
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-07-25 21:36:45 -0400

    feature:login menu and logout funct

--- Views/Assets/Messages.cs
+++ Views/Assets/Messages.cs
@@ -7,9 +7,16 @@
         private static readonly string _MAIN_MENU_OPTION_EXIT = "Exit(X)";
         private static readonly string _MAIN_MENU_OPTION_INVALID = "(Invalid option!)";
 
+
+        private static readonly string _MENU_LOGIN_LOGGED_IN = "User logged in";
+        private static readonly string _LOGIN_MENU_OPTION_LOGOUT = "Logout(B)";
+
         public static string MAIN_MENU_LOGIN_TITLE => _MAIN_MENU_LOGIN_TITLE;
         public static string MAIN_MENU_OPTION_LOGIN => _MAIN_MENU_OPTION_LOGIN;
         public static string MAIN_MENU_OPTION_EXIT => _MAIN_MENU_OPTION_EXIT;
         public static string MAIN_MENU_OPTION_INVALID => _MAIN_MENU_OPTION_INVALID;
+
+        public static string MENU_LOGIN_LOGGED_IN => _MENU_LOGIN_LOGGED_IN;
+        public static string LOGIN_MENU_OPTION_LOGOUT => _LOGIN_MENU_OPTION_LOGOUT;
     }
 }
